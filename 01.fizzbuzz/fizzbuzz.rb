b8814703651522b80008b0ff44397d174e008669
yoshimoto <work.s.yoshimoto@gmail.com>
2024-02-18 22:09:26 +0900

    fizzbuzzの課題を作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+(1..20).each do |x|
+  if x % 3 == 0 && x % 5 == 0
+    puts "fizzbuzz"
+  elsif x % 3 == 0  
+    puts "fizz"
+  elsif x % 5 == 0
+    puts "buzz"
+  else
+    puts x
+  end
+end
