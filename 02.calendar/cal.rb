6c07dd23321a03b2a188863905a7a7845f8b216c
yoshimoto <work.s.yoshimoto@gmail.com>
2024-02-20 23:09:18 +0900

    月の最終日を取得する処理を変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -17,22 +17,25 @@ get_args = ARGV.getopts('y:m:')
 argv_year = get_args["y"] == nil ?  today.year : get_args["y"].to_i 
 argv_month = get_args["m"] == nil ? today.month : get_args["m"].to_i 
 
-target_date = Date.new(argv_year, argv_month)
+target_date = Date.new(argv_year, argv_month, 1)
+last_day = Date.new(argv_year, argv_month, -1).day
 
 puts "      #{argv_month}月 #{argv_year}"
 DAY_OF_WEEK.each_value do |day_of_week|
   print day_of_week + " "
 end
 puts ""
-(0..30).each do |x|
-  break if target_date.next_day(x).month != argv_month 
-  if x == 0 
-    target_date.next_day(x).wday.times do
+
+count = 0
+while last_day > count
+  if count == 0 
+    target_date.next_day(count).wday.times do
       print "   "
     end
   end
-  print target_date.next_day(x).day.to_s.ljust(3)
-  puts "" if target_date.next_day(x).cwday == 6
+  print target_date.next_day(count).day.to_s.ljust(3)
+  puts "" if target_date.next_day(count).cwday == 6
+  count += 1
 end
 
 puts "\n\n"
