c685c1961a8de9866f36fba7ca7685010760e1ef
yoshimoto <work.s.yoshimoto@gmail.com>
2024-02-24 01:47:26 +0900

    rubocopの指摘箇所を 修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
 (1..20).each do |x|
-  if x % 3 == 0 && x % 5 == 0
-    puts "fizzbuzz"
-  elsif x % 3 == 0  
-    puts "fizz"
-  elsif x % 5 == 0
-    puts "buzz"
+  if (x % 15).zero?
+    puts 'fizzbuzz'
+  elsif (x % 3).zero?
+    puts 'fizz'
+  elsif (x % 5).zero?
+    puts 'buzz'
   else
     puts x
   end
