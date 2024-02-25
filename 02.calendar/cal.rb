cab207bf53a057f3ba15d87af65d044af537c638
yoshimoto <work.s.yoshimoto@gmail.com>
2024-02-25 19:44:34 +0900

    1回目レビュー後の修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,40 +2,24 @@
 require 'optparse'
 require 'date'
 
-DAY_OF_WEEK = { 
-  0 => "日" ,  
-  1 => "月", 
-  2 => "火", 
-  3 => "水", 
-  4 => "木", 
-  5 => "金", 
-  6 => "土",
-}
+DAY_OF_WEEK = ["日", "月", "火", "水", "木", "金", "土"] 
 
 today = Date.today
-get_args = ARGV.getopts('y:m:')
-argv_year = get_args["y"] == nil ?  today.year : get_args["y"].to_i 
-argv_month = get_args["m"] == nil ? today.month : get_args["m"].to_i 
+args_yaer_month = ARGV.getopts('y:m:')
+argv_year = args_yaer_month["y"]&.to_i || today.year
+argv_month = args_yaer_month["m"]&.to_i ||today.month
 
-target_date = Date.new(argv_year, argv_month, 1)
+first_date = Date.new(argv_year, argv_month, 1)
 last_day = Date.new(argv_year, argv_month, -1).day
 
 puts "      #{argv_month}月 #{argv_year}"
-DAY_OF_WEEK.each_value do |day_of_week|
+DAY_OF_WEEK.each do |day_of_week|
   print day_of_week + " "
 end
 puts ""
-
-count = 0
-while last_day > count
-  if count == 0 
-    target_date.next_day(count).wday.times do
-      print "   "
-    end
-  end
-  print target_date.next_day(count).day.to_s.rjust(2) + " "
-  puts "" if target_date.next_day(count).cwday == 6
-  count += 1
+print "   " * first_date.next_day(0).wday
+(0...(last_day -1)).each do |x|
+  print first_date.next_day(x).day.to_s.rjust(2) + " "
+  puts "" if first_date.next_day(x).cwday == 6
 end
-
 puts "\n\n"
