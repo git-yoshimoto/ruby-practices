e21646a28c8481035e40b255fcf0dcc2e9ef51c2
yoshimoto <work.s.yoshimoto@gmail.com>
2024-03-06 23:51:52 +0900

    レビュー後のリファクタリングを実施

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,8 +4,9 @@
 score = ARGV[0]
 scores = score.split(',')
 shots = []
+FINAL_FRAME_SHOTS_REACHED = 18
 scores.each do |s|
-  if shots.length >= 18
+  if shots.length >= FINAL_FRAME_SHOTS_REACHED
     shots << (s == 'X' ? 10 : s.to_i)
   elsif s == 'X'
     shots << 10
@@ -27,20 +28,22 @@ end
 point = 0
 frames.each_with_index do |frame, x|
   point += frame.sum
+  next if x > 8 || frame.sum != 10
+
+  # スペアの場合
+  if frame[0] != 10
+    point += frames[x + 1][0]
+    next
+  end
+
+  # ストライクの場合
   if x < 8
     # 1〜8フレームの処理
-    if frame[0] == 10
-      point += frames[x + 1][0] + (frames[x + 1][0] == 10 ? frames[x + 2][0] : frames[x + 1][1])
-    elsif frame.sum == 10
-      point += frames[x + 1][0]
-    end
-  elsif x == 8
-    # 9フレームの処理
-    if frame[0] == 10
-      point += frames[x + 1][0] + frames[x + 1][1]
-    elsif frame.sum == 10
-      point += frames[x + 1][0]
-    end
+    point += frames[x + 1][0] + (frames[x + 1][0] == 10 ? frames[x + 2][0] : frames[x + 1][1])
+    next
   end
+
+  # 9フレームの処理
+  point += frames[x + 1][0] + frames[x + 1][1]
 end
 puts point
